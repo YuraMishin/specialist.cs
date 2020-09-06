e555afea15a351b018b856d3b566acb0048721d4
Mishin Yura <mishin.inbox@gmail.om>
2020-09-06 14:24:30 +0300

    Exercise 1: Building a Simple Console Application

---
+++ Labfiles/Lab 1/Ex1/Starter/ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Replace(",", " y:");
+                line = "x:" + line;
+                Console.WriteLine(line);
+            }
+        }
+    }
+
+}
