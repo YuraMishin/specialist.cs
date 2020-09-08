babd8d17e3aa49ecb6bfcfda000bae74b4b63152
Mishin Yura <mishin.inbox@gmail.com>
2020-09-08 09:18:33 +0300

    Converting Integer Numeric Data to Binary

---
+++ Labfiles/Lab 2/Ex2/IntegerToBinary/IntegerToBinary/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+
+namespace IntegerToBinary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Constructor for Main Window
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Convert an integer to binary
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            int userInput;
+            if (!int.TryParse(inputTextBox.Text, out userInput))
+            {
+                MessageBox.Show("Textbox does not contain an integer");
+                return;
+            }
+
+            if (userInput < 0)
+            {
+                MessageBox.Show("Please enter a positive number or zero");
+                return;
+            }
+
+            int reminder;
+            var result = new StringBuilder();
+            do
+            {
+                reminder = userInput % 2;
+                userInput /= 2;
+                result.Insert(0, reminder);
+
+            } while (userInput > 0);
+
+            binaryLabel.Content = result.ToString();
+        }
+    }
+}
