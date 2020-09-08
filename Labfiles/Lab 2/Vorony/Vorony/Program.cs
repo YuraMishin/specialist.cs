f1ed4488bf2551fb89b08dfc9c3524f71a4cccbf
Mishin Yura <mishin.inbox@gmail.com>
2020-09-08 08:10:48 +0300

    Подсчёт ворон

---
+++ Labfiles/Lab 2/Vorony/Vorony/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Vorony
+{
+    /// <summary>
+    /// Программа выводит количество ворон на ветке
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Точка входа приложения
+        /// </summary>
+        static void Main()
+        {
+            Console.Write("Введите число ворон на ветке: ");
+            var ravenQ = int.Parse(Console.ReadLine());
+            string result;
+            var rem100 = ravenQ % 100;
+            if (rem100 >= 11 && rem100 <= 14)
+            {
+                result = "ворон";
+            }
+            else
+            {
+                var rem10 = ravenQ % 10;
+                switch (rem10)
+                {
+                    case 1:
+                        result = "ворона";
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        result = "вороны";
+                        break;
+                    default:
+                        result = "ворон";
+                        break;
+                }
+            }
+            Console.WriteLine($"На ветке {ravenQ} {result}");
+            Console.ReadKey();
+        }
+    }
+}
