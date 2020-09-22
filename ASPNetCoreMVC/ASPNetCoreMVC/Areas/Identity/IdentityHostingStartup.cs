925ea98604a17de28dd5f8a615bc0ac4fdf06e27
Mishin Yura <mishin.inbox@gmail.com>
2020-09-22 12:34:27 +0300

    Added scafold pages for Identity. Closes #31

---
+++ ASPNetCoreMVC/ASPNetCoreMVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ASPNetCoreMVC.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASPNetCoreMVC.Areas.Identity.IdentityHostingStartup))]
+namespace ASPNetCoreMVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
