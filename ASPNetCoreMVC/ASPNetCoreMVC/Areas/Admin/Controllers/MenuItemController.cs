ed0df9bac4127cb0cab5e66ea5133846fc4b1f02
Mishin Yura <mishin.inbox@gmail.com>
2020-09-29 19:46:23 +0300

    Added MenuItem Edit feature. Closes #47

--- ASPNetCoreMVC/ASPNetCoreMVC/Areas/Admin/Controllers/MenuItemController.cs
+++ ASPNetCoreMVC/ASPNetCoreMVC/Areas/Admin/Controllers/MenuItemController.cs
@@ -5,9 +5,9 @@ using ASPNetCoreMVC.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASPNetCoreMVC.Areas.Admin.Controllers
@@ -85,7 +85,7 @@ namespace ASPNetCoreMVC.Areas.Admin.Controllers
         .ToInt32(Request.Form["SubCategoryId"].ToString());
       if (!ModelState.IsValid)
       {
-        MenuItemVM.Categories = _db.Categories;
+        MenuItemVM.Categories = await _db.Categories.ToListAsync();
         return View(MenuItemVM);
       }
 
@@ -96,7 +96,7 @@ namespace ASPNetCoreMVC.Areas.Admin.Controllers
       var files = HttpContext.Request.Form.Files;
       var menuItenFromDb =
         await _db.MenuItems.FindAsync(MenuItemVM.MenuItem.Id);
-      if (files.Any())
+      if (files.Count > 0)
       {
         var uploads = Path.Combine(webRootPath, "img");
         var extension = Path.GetExtension(files[0].FileName);
@@ -117,6 +117,95 @@ namespace ASPNetCoreMVC.Areas.Admin.Controllers
           webRootPath + @"\img\" + MenuItemVM.MenuItem.Id + ".png");
         menuItenFromDb.Image = @"\img\" + MenuItemVM.MenuItem.Id + ".png";
       }
+
+      await _db.SaveChangesAsync();
+      return RedirectToAction(nameof(Index));
+    }
+
+    /// <summary>
+    /// Method shows edit menu item UI
+    /// </summary>
+    /// <param name="id">Id</param>
+    /// <returns>IActionResult</returns>
+    public async Task<IActionResult> Edit(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      MenuItemVM.MenuItem = await _db.MenuItems
+        .Include(m => m.Category)
+        .Include(m => m.SubCategory)
+        .SingleOrDefaultAsync(m => m.Id == id);
+      MenuItemVM.SubCategories = await _db
+        .SubCategories
+        .Where(s => s.CategoryId == MenuItemVM.MenuItem.CategoryId)
+        .ToListAsync();
+      if (MenuItemVM.MenuItem == null)
+      {
+        return NotFound();
+      }
+      return View(MenuItemVM);
+    }
+
+    /// <summary>
+    /// Method edits menu item
+    /// </summary>
+    /// <param name="id">id</param>
+    /// <returns>IActionResult</returns>
+    [HttpPost, ActionName("Edit")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> EditPOST(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      MenuItemVM.MenuItem.SubCategoryId = Convert
+        .ToInt32(Request.Form["SubCategoryId"].ToString());
+      if (!ModelState.IsValid)
+      {
+        MenuItemVM.Categories = await _db.Categories.ToListAsync();
+        MenuItemVM.SubCategories = await _db.SubCategories
+          .Where(s => s.CategoryId == MenuItemVM.MenuItem.CategoryId)
+          .ToListAsync();
+        return View(MenuItemVM);
+      }
+      // img saving
+      var webRootPath = _hostingEnvironment.WebRootPath;
+      var files = HttpContext.Request.Form.Files;
+      var menuItenFromDb =
+        await _db.MenuItems.FindAsync(MenuItemVM.MenuItem.Id);
+      if (files.Count > 0)
+      {
+        var uploads = Path.Combine(webRootPath, "img");
+        var extension_new = Path.GetExtension(files[0].FileName);
+        var imagePath = Path.Combine(
+          webRootPath,
+          menuItenFromDb.Image.TrimStart('\\'));
+        if (System.IO.File.Exists(imagePath))
+        {
+          System.IO.File.Delete(imagePath);
+        }
+
+        using (var filesStream
+          = new FileStream(
+            Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension_new),
+            FileMode.Create))
+        {
+          files[0].CopyTo(filesStream);
+        }
+
+        menuItenFromDb.Image = @"\img\" + MenuItemVM.MenuItem.Id + extension_new;
+      }
+      menuItenFromDb.Name = MenuItemVM.MenuItem.Name;
+      menuItenFromDb.Description = MenuItemVM.MenuItem.Description;
+      menuItenFromDb.Price = MenuItemVM.MenuItem.Price;
+      menuItenFromDb.Spicyness = MenuItemVM.MenuItem.Spicyness;
+      menuItenFromDb.CategoryId = MenuItemVM.MenuItem.CategoryId;
+      menuItenFromDb.SubCategoryId = MenuItemVM.MenuItem.SubCategoryId;
 
       await _db.SaveChangesAsync();
       return RedirectToAction(nameof(Index));
