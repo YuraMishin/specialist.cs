9ce6e2c1037195f3b636e1bf1093b06d0d6eaa1e
Mishin Yura <mishin.inbox@gmail.com>
2020-09-30 10:08:10 +0300

    Added Coupon edit feature. Closes #52

--- ASPNetCoreMVC/ASPNetCoreMVC/Areas/Admin/Controllers/CouponController.cs
+++ ASPNetCoreMVC/ASPNetCoreMVC/Areas/Admin/Controllers/CouponController.cs
@@ -3,6 +3,7 @@ using ASPNetCoreMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASPNetCoreMVC.Areas.Admin.Controllers
@@ -74,6 +75,75 @@ namespace ASPNetCoreMVC.Areas.Admin.Controllers
         }
 
         _db.Coupons.Add(coupon);
+        await _db.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+      }
+
+      return View(coupon);
+    }
+
+    /// <summary>
+    /// Method shows UI to edit coupon
+    /// </summary>
+    /// <param name="id">id</param>
+    /// <returns>IActionResult</returns>
+    public async Task<IActionResult> Edit(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var coupon = await _db.Coupons.SingleOrDefaultAsync(m => m.Id == id);
+      if (coupon == null)
+      {
+        return NotFound();
+      }
+
+      return View(coupon);
+    }
+
+    /// <summary>
+    /// Method updates coupon
+    /// </summary>
+    /// <param name="coupon">Coupon</param>
+    /// <returns>IActionResult</returns>
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Coupon coupon)
+    {
+      if (coupon.Id == 0)
+      {
+        return NotFound();
+      }
+
+      var couponFromDb = await _db.Coupons
+        .Where(c => c.Id == coupon.Id)
+        .FirstOrDefaultAsync();
+
+      if (ModelState.IsValid)
+      {
+        var files = HttpContext.Request.Form.Files;
+        if (files.Count > 0)
+        {
+          byte[] p1 = null;
+          using (var fs1 = files[0].OpenReadStream())
+          {
+            using (var ms1 = new MemoryStream())
+            {
+              fs1.CopyTo(ms1);
+              p1 = ms1.ToArray();
+            }
+          }
+
+          couponFromDb.Picture = p1;
+        }
+
+        couponFromDb.MinimumAmount = coupon.MinimumAmount;
+        couponFromDb.Name = coupon.Name;
+        couponFromDb.Discount = coupon.Discount;
+        couponFromDb.CouponType = coupon.CouponType;
+        couponFromDb.isActive = coupon.isActive;
         await _db.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
       }
