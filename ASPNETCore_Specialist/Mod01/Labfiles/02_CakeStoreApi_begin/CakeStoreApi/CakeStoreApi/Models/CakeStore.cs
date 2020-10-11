9bd4bbf77e9047213ddf98a594c9b4b61904ec8d
Mishin Yura <mishin.inbox@gmail.com>
2020-10-11 08:35:51 +0300

    M1. 2. Explored a Web API Application. Closes #67

---
+++ ASPNETCore_Specialist/Mod01/Labfiles/02_CakeStoreApi_begin/CakeStoreApi/CakeStoreApi/Models/CakeStore.cs
@@ -0,0 +1,9 @@
+namespace CakeStoreApi.Models
+{
+  public class CakeStore
+  {
+    public int Id { get; set; }
+    public string CakeType { get; set; }
+    public int Quantity { get; set; }
+  }
+}
---
+++ ASPNETCore_Specialist/Mod01/Labfiles/02_CakeStoreApi_begin/CakeStoreApi/CakeStoreApi/Models/IData.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace CakeStoreApi.Models
+{
+  public interface IData
+  {
+    List<CakeStore> CakesList { get; set; }
+    List<CakeStore> CakesInitializeData();
+    CakeStore GetCakeById(int? id);
+  }
+}
