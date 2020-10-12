f8bdd1ad028f84b93dfa7ada4fab72f07e68914d
Mishin Yura <mishin.inbox@gmail.com>
2020-10-12 07:44:05 +0300

    M4 2. Configured Routes by Using the Routing Table. Closes #78

---
+++ ASPNETCore_Specialist/Mod04/01_WorldJourney/WorldJourney/Controllers/TravelerController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorldJourney.Controllers
+{
+  public class TravelerController : Controller
+  {
+    public IActionResult Index(string name)
+    {
+      ViewBag.VisiterName = name;
+      return View();
+    }
+  }
+}
--- ASPNETCore_Specialist/Mod04/01_WorldJourney/WorldJourney/Startup.cs
+++ ASPNETCore_Specialist/Mod04/01_WorldJourney/WorldJourney/Startup.cs
@@ -16,6 +16,22 @@ namespace WorldJourney
     {
       app.UseStaticFiles();
       app.UseMvcWithDefaultRoute();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+          name: "TravelerRoute",
+          template: "{controller}/{action}/{name}",
+          constraints: new { name = "[A-Za-z ]+" },
+          defaults: new
+          { controller = "Traveler", action = "Index", name = "Katie Bruce" });
+
+        routes.MapRoute(
+          name: "defaultRoute",
+          template: "{controller}/{action}/{id?}",
+          defaults: new { controller = "Home", action = "Index" },
+          constraints: new { id = "[0-9]+" });
+      });
     }
   }
 }
