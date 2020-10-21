7380891feb36e307ba547458d1d3c9a979ac5114
Mishin Yura <mishin.inbox@gmail.com>
2020-10-21 13:10:42 +0300

    NUnit. Create the Project. Closes #129

---
+++ NUnitTests/NUnitTests/UnitTest.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace NUnitTests
+{
+  /// <summary>
+  /// Class UnitTest.
+  /// Tests bla-bla-bla class
+  /// </summary>
+  public class UnitTest
+  {
+    /// <summary>
+    /// Tests bla-bla() method
+    /// </summary>
+    [Test]
+    public void Method_Condition_Result()
+    {
+      Assert.Fail();
+    }
+  }
+}
