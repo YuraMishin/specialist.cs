11079cc0eec648fa156382e77b7a902a5a6b3e5d
Mishin Yura <mishin.inbox@gmail.com>
2020-10-23 17:00:27 +0300

    Connected the Angular and ASP.NET Core Applications. Closes #138

--- SportsStore/ServerApp/ServerApp/Controllers/HomeController.cs
+++ SportsStore/ServerApp/ServerApp/Controllers/HomeController.cs
@@ -6,28 +6,30 @@ using System.Diagnostics;
 namespace ServerApp.Controllers
 {
   public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View("Placeholder");
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None,
+      NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel
+        {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+  }
 }
