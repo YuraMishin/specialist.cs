eea0fc052347d7c45530b7db1a45d195538a299c
Mishin Yura <mishin.inbox@gmail.com>
2020-10-26 11:29:21 +0300

    Created the ASP.NET Core MVC Data Model. Closes #144

--- SportsStore/ServerApp/Controllers/HomeController.cs
+++ SportsStore/ServerApp/Controllers/HomeController.cs
@@ -1,24 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using ServerApp.Models;
 using System.Diagnostics;
+using System.Linq;
 
-namespace ServerApp.Controllers {
+namespace ServerApp.Controllers
+{
+  public class HomeController : Controller
+  {
+    private DataContext context;
 
-    public class HomeController : Controller {
-
-        public IActionResult Index() {
-            return View();
-        }
-
-        public IActionResult Privacy() {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, 
-            NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id 
-                ?? HttpContext.TraceIdentifier });
-        }
+    public HomeController(DataContext ctx)
+    {
+      context = ctx;
     }
+
+    public IActionResult Index()
+    {
+      return View(context.Products.First());
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None,
+      NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel
+      {
+        RequestId = Activity.Current?.Id
+                    ?? HttpContext.TraceIdentifier
+      });
+    }
+  }
 }
