4b70630fadeeef1cdfd62e2a0ce8aa7f1f0b5edc
Mishin Yura <mishin.inbox@gmail.com>
2020-10-27 12:01:11 +0300

    Used ASP.NET MVC. Closes #146

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -1,6 +1,8 @@
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace DutchTreat
 {
@@ -10,14 +12,30 @@ namespace DutchTreat
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddTransient<IMailService, NullMailService>();
+
+      services.AddControllersWithViews();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      app.UseDefaultFiles();
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
       app.UseStaticFiles();
       app.UseNodeModules();
+
+      app.UseRouting();
+
+      app.UseEndpoints(cfg =>
+      {
+        cfg.MapControllerRoute("Default",
+          "{controller}/{action}/{id?}",
+          new {controller = "App", Action = "Index"});
+      });
     }
   }
 }
