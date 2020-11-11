19dd9c811ce28b3927cf2c3aef9d96f588e3f381
Mishin Yura <mishin.inbox@gmail.com>
2020-11-11 12:27:37 +0300

    Added MS SQL database connection. Closes #171

--- BookShop/backend/MVC/Data/ApplicationDbContext.cs
+++ BookShop/backend/MVC/Data/ApplicationDbContext.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MVC.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+  public class ApplicationDbContext : IdentityDbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
     }
+  }
 }
