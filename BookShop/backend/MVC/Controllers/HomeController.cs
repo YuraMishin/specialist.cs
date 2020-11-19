10f81ea68c819c7b99d782429bd2ac4924f08e4b
Mishin Yura <mishin.inbox@gmail.com>
2020-11-19 08:06:52 +0300

    Added design example docs. Closes #180

--- BookShop/backend/MVC/Controllers/HomeController.cs
+++ BookShop/backend/MVC/Controllers/HomeController.cs
@@ -1,37 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MVC.Models;
 
 namespace MVC.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
+
+    public IActionResult Index()
+    {
+      _logger.LogInformation("App is ready");
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None,
+      NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel
+        {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+  }
 }
