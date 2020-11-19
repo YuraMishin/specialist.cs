448fdf7be6f10d9ae6c434ce24aad207f29907cf
Mishin Yura <mishin.inbox@gmail.com>
2020-11-19 17:29:49 +0300

    Added Categories table seeder. Closes #185

---
+++ BookShop/backend/MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MVC.Controllers;
+using MVC.Data;
+
+namespace MVC.Areas.Admin.Controllers
+{
+  /// <summary>
+  /// Class implements Category Controller
+  /// </summary>
+  [Area("Admin")]
+  public class CategoryController : Controller
+  {
+    /// <summary>
+    /// DbContext
+    /// </summary>
+    private readonly ApplicationDbContext _db;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="db">Db Context</param>
+    public CategoryController(ApplicationDbContext db)
+    {
+      this._db = db;
+    }
+
+    /// <summary>
+    /// Method retrieves all categories
+    /// </summary>
+    /// <returns>IActionResult</returns>
+    public async Task<IActionResult> Index()
+    {
+      var categories = await _db.Categories.ToListAsync();
+
+      return Json(categories);
+    }
+  }
+}
---
+++ BookShop/backend/MVC/Migrations/20201119132503_SeedCategoriesTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC.Migrations
+{
+  public partial class SeedCategoriesTable : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      // PostgreSQL
+      // migrationBuilder.Sql(
+      //   @"insert into ""Categories"" (""Name"") VALUES ('Adventures')");
+
+      migrationBuilder.InsertData(
+        table: "Categories",
+        columns: new[] {"Name"},
+        values: new object[] {"Adventures"});
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      // PostgreSQL
+      // migrationBuilder
+      //   .Sql(@"delete from ""Categories"" where ""Name"" in ('Adventures')");
+
+      migrationBuilder.DeleteData(
+        table: "Categories",
+        keyColumn: "Name",
+        keyValue: "Adventures");
+    }
+  }
+}
