407624d712a29ce9f22e59f384750ff195b5a9fb
Mishin Yura <mishin.inbox@gmail.com>
2020-11-27 15:12:17 +0300

    Added Coupons UI. All Coupons. Closes #216

---
+++ BookShop/backend/MVC/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+
+namespace MVC.Areas.Admin.Controllers
+{
+  /// <summary>
+  /// Class implements Coupon Controller
+  /// </summary>
+  [Area("Admin")]
+  public class CouponController : Controller
+  {
+    /// <summary>
+    /// DbContext
+    /// </summary>
+    private readonly ApplicationDbContext _db;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="db"></param>
+    public CouponController(ApplicationDbContext db)
+    {
+      _db = db;
+    }
+
+    /// <summary>
+    /// Method shows UI to list all coupons.
+    /// GET: /admin/coupon/
+    /// </summary>
+    /// <returns>IActionResult</returns>
+    public async Task<IActionResult> Index()
+    {
+      var coupons = await _db.Coupons.ToListAsync();
+      return View(coupons);
+    }
+  }
+}
