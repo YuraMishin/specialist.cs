a7a097679fc360d7a13cd0aa4eca9cc3526d82dd
Mishin Yura <mishin.inbox@gmail.com>
2020-11-27 16:23:19 +0300

    Added Coupon Edit feature. Closes #220

--- BookShop/backend/MVC/Areas/Admin/Controllers/CouponController.cs
+++ BookShop/backend/MVC/Areas/Admin/Controllers/CouponController.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -103,6 +104,55 @@ namespace MVC.Areas.Admin.Controllers
       if (coupon == null)
       {
         return NotFound();
+      }
+
+      return View(coupon);
+    }
+
+    /// <summary>
+    /// Method updates coupon.
+    /// POST: /admin/coupon/edit/id
+    /// </summary>
+    /// <param name="coupon">Coupon</param>
+    /// <returns>IActionResult</returns>
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Coupon coupon)
+    {
+      if (coupon.Id == 0)
+      {
+        return NotFound();
+      }
+
+      var couponFromDb = await _db.Coupons
+        .Where(c => c.Id == coupon.Id)
+        .FirstOrDefaultAsync();
+
+      if (ModelState.IsValid)
+      {
+        var files = HttpContext.Request.Form.Files;
+        if (files.Count > 0)
+        {
+          byte[] p1 = null;
+          using (var fs1 = files[0].OpenReadStream())
+          {
+            using (var ms1 = new MemoryStream())
+            {
+              fs1.CopyTo(ms1);
+              p1 = ms1.ToArray();
+            }
+          }
+
+          couponFromDb.Picture = p1;
+        }
+
+        couponFromDb.MinimumAmount = coupon.MinimumAmount;
+        couponFromDb.Name = coupon.Name;
+        couponFromDb.Discount = coupon.Discount;
+        couponFromDb.CouponType = coupon.CouponType;
+        couponFromDb.IsActive = coupon.IsActive;
+        await _db.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
       }
 
       return View(coupon);
