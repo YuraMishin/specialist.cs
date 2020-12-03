67ee4e0af32e0c3ebb8a8bf692c29b85087f2431
Mishin Yura <mishin.inbox@gmail.com>
2020-12-03 09:19:22 +0300

    Implemented user lock | unlock feature. Closes #227

--- BookShop/backend/MVC/Areas/Admin/Controllers/UserController.cs
+++ BookShop/backend/MVC/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using MVC.Data;
+﻿using System;
+using MVC.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
@@ -34,11 +35,67 @@ namespace MVC.Areas.Admin.Controllers
     /// <returns>IActionResult</returns>
     public async Task<IActionResult> Index()
     {
-      var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+      var claimsIdentity = (ClaimsIdentity) this.User.Identity;
       var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
       return View(await _db.ApplicationUser.Where(u => u.Id != claim.Value)
         .ToListAsync());
+    }
+
+    /// <summary>
+    /// Method locks user.
+    /// GET: /admin/user/lock/id
+    /// </summary>
+    /// <param name="id">Id</param>
+    /// <returns>IActionResult</returns>
+    public async Task<IActionResult> Lock(string id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var applicationUser =
+        await _db.ApplicationUser.FirstOrDefaultAsync(m => m.Id == id);
+
+      if (applicationUser == null)
+      {
+        return NotFound();
+      }
+
+      applicationUser.LockoutEnd = DateTime.Now.AddYears(1000);
+
+      await _db.SaveChangesAsync();
+
+      return RedirectToAction(nameof(Index));
+    }
+
+    /// <summary>
+    /// Method unlock user.
+    /// GET: /admin/user/unlock/id
+    /// </summary>
+    /// <param name="id">Id</param>
+    /// <returns>IActionResult</returns>
+    public async Task<IActionResult> UnLock(string id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var applicationUser =
+        await _db.ApplicationUser.FirstOrDefaultAsync(m => m.Id == id);
+
+      if (applicationUser == null)
+      {
+        return NotFound();
+      }
+
+      applicationUser.LockoutEnd = DateTime.Now;
+
+      await _db.SaveChangesAsync();
+
+      return RedirectToAction(nameof(Index));
     }
   }
 }
